d726581ce190191328b17d8548be13cfa1a42085
bogdanpoh <bogdan20205@gmail.com>
2021-01-17 12:21:13 +0200

    set infinity polling for admin bot

--- admin_bot.py
+++ admin_bot.py
@@ -188,7 +188,7 @@ def process_remove_session_by_group_id(message):
 
 def main():
     try:
-        bot.polling(none_stop=True, interval=0)
+        bot.infinity_polling(True)
     except Exception as ex:
         print(str(ex))
         bot.send_message(constants.admin_chat_id, "Admin bot is off..")
